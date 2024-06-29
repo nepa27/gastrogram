d96e925391f4c918f51147cf529345cdfbc10168
nepa27 <nepalek@mail.ru>
2024-06-29 20:00:07 +0300

    Create project, apps

---
+++ backend/recipes/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+class Recipes(models.Model):
+    pass
+
+
+class Tags(models.Model):
+    pass
+
+
+class Ingredients(models.Model):
+    pass
+
+
+class FavoriteRecipes(models.Model):
+    pass
+
+
+class Subscriptions(models.Model):
+    pass
+
+
+class ListOfRecipes(models.Model):
+    pass
