c271c2e7d0b0e26f85ed1eb33dd4b914c09364db
nepa27 <nepalek@mail.ru>
2024-07-24 14:51:36 +0300

    Исправил путь к статике

--- backend/recipes_backend/settings.py
+++ backend/recipes_backend/settings.py
@@ -101,9 +101,9 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'collected_static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/static/media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
